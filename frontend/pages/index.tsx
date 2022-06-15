8d6d9d499950f83ee899f6a655e1edcb2df62bb2
Jonatan Hanson <jonatan.hanson@gmail.com>
2022-06-15 16:16:33 +0200

    tailwind

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -12,14 +12,11 @@ export default function Home(): JSX.Element {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+
+        <h1 className="text-3xl font-bold underline text-orange-500">
+          Hello world!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>

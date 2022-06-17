e55b422d577e586eddbbedfef4d14e64292c2173
MasterKenth <MasterKenth@users.noreply.github.com>
2022-06-17 09:52:16 +0200

    ✨ feat: use backend for rewards

--- frontend/pages/awardcenter.tsx
+++ frontend/pages/awardcenter.tsx
@@ -1,7 +1,9 @@
 import Layout from "../components/Layout";
-import Link from 'next/link'
+import Link from "next/link";
+import { useContext } from "react";
+import { PointsProgramContext } from "../services/pointsProgram/pointsProgram";
 
-const awards = [
+/* const awards = [
   {
     name: "ICA Maxi",
     points: 100,
@@ -14,13 +16,14 @@ const awards = [
     name: "Busskort",
     points: 400,
   }
-];
+]; */
 
 function Awardcenter() {
+  const { rewards } = useContext(PointsProgramContext);
+
   return (
     <Layout>
       <div className="m-4 rounded-xl bg-white shadow-md">
-
         <div className="border-b-4 border-green p-3">
           <h2 className="text-md font-semibold text-neutral-800">
             Belöningscenter
@@ -28,10 +31,12 @@ function Awardcenter() {
         </div>
 
         <div>
-          {awards.map((award) => (
+          {rewards.map((award) => (
             <Link href={`/award/${award.name}`} key={award.name}>
-              <div className="border-b-2 border-neutral-200 p-4 flex justify-between text-neutral-800 bg-white
-            ">
+              <div
+                className="border-b-2 border-neutral-200 p-4 flex justify-between text-neutral-800 bg-white
+            "
+              >
                 <p className="text-xs font-semibold ">{award.name}</p>
                 <p className="text-xs font-bold ">{award.points}p</p>
               </div>
@@ -40,13 +45,9 @@ function Awardcenter() {
 
           <div className="p-4">
             <Link href="/">
-              <span className="text-green font-bold">
-                Tillbaka
-              </span>
+              <span className="text-green font-bold">Tillbaka</span>
             </Link>
           </div>
-
-
         </div>
       </div>
     </Layout>

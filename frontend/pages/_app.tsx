5aa4bf448b3b050ae742806e26ea30c146601c66
MasterKenth <MasterKenth@users.noreply.github.com>
2022-06-17 13:38:18 +0200

    ✨ feat: use graphql backend

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import PointsProgramProvider from "../services/pointsProgram/pointsProgram";
-import jsonBackend from "../services/pointsProgram/jsonBackend";
+import graphqlBackend from "../services/pointsProgram/graphqlBackend";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PointsProgramProvider provider={jsonBackend}>
+    <PointsProgramProvider provider={graphqlBackend}>
       <Component {...pageProps} />
     </PointsProgramProvider>
   );
